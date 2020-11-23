f3e3e6b57cf8782264766a1cdbb1e5472825bda4
Hayden Housen <hayden@haydenhousen.com>
2020-11-23 10:41:34 -0500

    Update div to floor_divide due to update to pytorch 1.7.0

--- presumm/modeling_bertabs.py
+++ presumm/modeling_bertabs.py
@@ -908,7 +908,7 @@ class Translator(object):
             topk_log_probs = topk_scores * length_penalty
 
             # Resolve beam origin and true word ids.
-            topk_beam_index = topk_ids.div(vocab_size)
+            topk_beam_index = topk_ids.floor_divide(vocab_size)
             topk_ids = topk_ids.fmod(vocab_size)
 
             # Map beam_index to batch_index in the flat representation.
